c283b38416989920c8e3bfc9d010c13369f50b05
rbbelka <rbbelka@gmail.com>
2016-03-31 17:53:31 +0300

    control work 1

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+
 int main()
 {
-    // TODO
+    size_t size = -1;
+    size_t mem_max = 0;
+
+    while (size > 0) {
+        char* res = (char*) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+        if (res != (char*) -1)
+            mem_max += size;
+        size /= 2;
+    }
+    
+    printf("Max size = %zu\n", mem_max);
     return 0;
 }
