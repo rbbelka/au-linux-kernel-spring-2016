b86ccca92a77d2ec603e9f0c64cfc60598dadec3
rbbelka <rbbelka@gmail.com>
2016-04-06 23:22:57 +0300

    vsd2 task

--- tasks/vsd2/vsd_driver/module.c
+++ tasks/vsd2/vsd_driver/module.c
@@ -109,7 +109,7 @@ static long vsd_ioctl_get_size(vsd_ioctl_get_size_arg_t __user *uarg)
 static long vsd_ioctl_set_size(vsd_ioctl_set_size_arg_t __user *uarg)
 {
     vsd_ioctl_set_size_arg_t arg;
-    if (0/* TODO device is currently mapped */)
+    if (vsd_dev->mmap_count > 0)
         return -EBUSY;
 
     if (copy_from_user(&arg, uarg, sizeof(arg)))
@@ -155,21 +155,25 @@ static struct vm_operations_struct vsd_dev_vma_ops = {
 
 static int map_vmalloc_range(struct vm_area_struct *uvma, void *kaddr, size_t size)
 {
+    struct page *current_page;
+    int ret = 0;
+    
     unsigned long uaddr = uvma->vm_start;
     if (!PAGE_ALIGNED(uaddr) || !PAGE_ALIGNED(kaddr)
             || !PAGE_ALIGNED(size))
         return -EINVAL;
 
-    /* 
-     * Remember that all the work with memory is done using pages.
-     * PAGE_SIZE is minimal size of memory we can map/unmap
-     * anywhere.
-     * Note that vmalloced VSD address range is not physically
-     * continuous. So we need to map each vmalloced page separetely.
-     * Use vmalloc_to_page and vm_insert_page functions for this.
-     */
-    // TODO
+    while (size > 0) {
+        current_page = vmalloc_to_page(kaddr);
+        ret = vm_insert_page(uvma, uaddr, current_page);
+        if (ret)
+            return ret;        
 
+        size  -= PAGE_SIZE;
+        kaddr += PAGE_SIZE;
+        uaddr += PAGE_SIZE;
+    }
+    
     uvma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;
     return 0;
 }
--- tasks/vsd2/vsd_userspace/vsd_device.c
+++ tasks/vsd2/vsd_userspace/vsd_device.c
@@ -1,51 +1,68 @@
 #include <sys/mman.h>
-
+#include <fcntl.h>
+#include <stdlib.h>
 #include "vsd_device.h"
+#include "../vsd_driver/vsd_ioctl.h"
+
+static int dev;
 
 int vsd_init()
 {
-    // TODO
-    return -1;
+    dev = open("/dev/vsd", O_RDWR);
+    return dev == -1 ? EXIT_FAILURE : EXIT_SUCCESS;;
 }
 
 int vsd_deinit()
 {
-    // TODO
-    return -1;
+    return close(dev);
 }
 
 int vsd_get_size(size_t *out_size)
 {
-    // TODO
-    return -1;
+    vsd_ioctl_get_size_arg_t get_size;
+    if (ioctl(dev, VSD_IOCTL_GET_SIZE, &get_size) == -1)
+        return EXIT_FAILURE;
+
+    *out_size = get_size.size;
+    return EXIT_SUCCESS;
 }
 
 int vsd_set_size(size_t size)
 {
-    // TODO
-    return -1;
+    vsd_ioctl_set_size_arg_t set_size;
+    set_size.size = size;
+    return ioctl(dev, VSD_IOCTL_SET_SIZE, &set_size) == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
-ssize_t vsd_read(char* dst, off_t offset, size_t size)
+//кажется, что читать должны из src, а писать в dst.
+
+ssize_t vsd_read(char* src, off_t offset, size_t size)
 {
-    // TODO
-    return -1;
+    if (lseek(dev, offset, SEEK_SET) == -1)
+        return EXIT_FAILURE;
+
+    return read(dev, src, size);
 }
 
-ssize_t vsd_write(const char* src, off_t offset, size_t size)
+ssize_t vsd_write(const char* dst, off_t offset, size_t size)
 {
-    // TODO
-    return -1;
+    if (lseek(dev, offset, SEEK_SET) == -1)
+        return EXIT_FAILURE;
+
+    return read(dev, dst, size);
 }
 
 void* vsd_mmap(size_t offset)
 {
-    // TODO
-    return MAP_FAILED;
+    size_t size = 0;
+    vsd_get_size(&size);
+    return mmap(NULL, size - offset, PROT_READ | PROT_WRITE, MAP_SHARED, dev, offset);
 }
 
 int vsd_munmap(void* addr, size_t offset)
 {
-    // TODO
-    return -1;
+    size_t size = 0;
+    vsd_get_size(&size);
+    return munmap(addr, size - offset);
 }
+
